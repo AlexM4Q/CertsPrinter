bba3a4340a4d43f1695c8741999f92f3a3b6864a
zhelonkin <Zhelonkin.ru@gmail.com>
2021-05-07 00:50:26 +0400

    Initial commit

---
+++ App/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace App
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            foreach (var value in Enum.GetValues(typeof(StoreName)).Cast<StoreName>())
+            {
+                var store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+                store.Open(OpenFlags.ReadOnly);
+
+                foreach (var certificate in store.Certificates)
+                {
+                    Console.WriteLine($"{value} {certificate.Thumbprint}");
+                }
+
+                store.Close();
+            }
+        }
+    }
+}
