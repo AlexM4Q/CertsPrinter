3f4bc25028daad5d92b1c3201c0674e3588b521a
zhelonkin <Zhelonkin.ru@gmail.com>
2021-05-07 01:00:29 +0400

    Вдогонку

--- App/Program.cs
+++ App/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 
@@ -6,16 +8,22 @@ namespace App
 {
     internal static class Program
     {
-        private static void Main()
+        private static void Main() => File.WriteAllLines(
+            "certs.csv",
+            Get().Select(x => $"{x.location}; {x.name}; {x.cert}")
+        );
+
+        private static IEnumerable<(StoreLocation location, StoreName name, string cert)> Get()
         {
-            foreach (var value in Enum.GetValues(typeof(StoreName)).Cast<StoreName>())
+            foreach (var location in Enum.GetValues(typeof(StoreLocation)).Cast<StoreLocation>())
+            foreach (var name in Enum.GetValues(typeof(StoreName)).Cast<StoreName>())
             {
-                var store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+                var store = new X509Store(name, location);
                 store.Open(OpenFlags.ReadOnly);
 
                 foreach (var certificate in store.Certificates)
                 {
-                    Console.WriteLine($"{value} {certificate.Thumbprint}");
+                    yield return (location, name, certificate.Thumbprint);
                 }
 
                 store.Close();
